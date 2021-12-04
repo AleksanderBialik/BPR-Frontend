927015ee6ea06c19444f75933c8096f651c38d57
Aleksander Bialik <aleksander.bialik97@gmail.com>
2021-12-04 14:49:17 +0100

    Env url change

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 
-export const URL = "https://ec2-3-8-48-95.eu-west-2.compute.amazonaws.com:443";
+export const URL =
+  process.env.NODE_ENV === "development"
+    ? "http://ec2-3-8-48-95.eu-west-2.compute.amazonaws.com:3000"
+    : "https://ec2-3-8-48-95.eu-west-2.compute.amazonaws.com:443";
+
+// export const URL = "https://ec2-3-8-48-95.eu-west-2.compute.amazonaws.com:443";
 
 const instance = axios.create({
   baseURL: URL,
